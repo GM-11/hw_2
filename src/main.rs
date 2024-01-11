7e7ff277f235f0a6757b7c00e18314b924c68efc
GM-11 <mathuyrgopal@gmail.com>
2024-01-12 00:49:24 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+struct WordCounter {
+    text: String,
+}
+
+impl WordCounter {
+    fn new(text: &str) -> WordCounter {
+        WordCounter {
+            text: String::from(text),
+        }
+    }
+
+    fn count_words(&self) -> usize {
+        self.text.split_whitespace().count()
+    }
+}
+
+fn main() {
+    println!("Enter a text:");
+
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Failed to read line");
+
+    let word_counter = WordCounter::new(input_text.trim());
+
+    let word_count = word_counter.count_words();
+    println!("Word count: {}", word_count);
+}
